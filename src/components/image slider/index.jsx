30e4f38c926f1c69d079c7d180311cadd54ae59b
samarth-mathur12 <samarthmathur199@gmail.com>
2025-07-26 04:26:59 +0530

    Image slider Component initalized

---
+++ src/components/image slider/index.jsx
@@ -0,0 +1,13 @@
+
+export default function ImageSlider(){
+
+    return(
+        <>
+            <h1>Image Slider</h1>
+            <div>
+                
+            </div>
+        </>
+    )
+
+}
